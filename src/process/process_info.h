dd2cf937c8b71479558f550fb5efbfb4c2bdb733
Guilherme <gulhermejacinto1@gmail.com>
2025-01-14 21:41:34 -0300

    logSystem

---
+++ src/process/process_info.h
@@ -0,0 +1,8 @@
+//
+// Created by guilherme on 14/01/25.
+//
+
+#ifndef PROCESS_INFO_H
+#define PROCESS_INFO_H
+
+#endif //PROCESS_INFO_H
---
+++ src/process/process_log.h
@@ -0,0 +1,8 @@
+//
+// Created by guilherme on 14/01/25.
+//
+
+#ifndef PROCESS_LOG_H
+#define PROCESS_LOG_H
+
+#endif //PROCESS_LOG_H
